f90feae574042350262a4c7cfa8057a9b2e2d602
value7 <franz.brandl1@gmx.net>
2017-02-03 20:55:31 +0100

    added redux and react-router

---
+++ src/containers/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+          <li><Link to="/Projects">About</Link></li>
+          <li><Link to="/About">Repos</Link></li>
+          <li><Link to="/App">reddit redux async</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
 
-ReactDOM.render(
-  <App />,
+import Projects from './components/Projects';
+import Header from './containers/Header';
+import About from './components/About';
+
+//router stuff
+import { Router, Route, browserHistory } from 'react-router';
+
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Header}>
+        <Route path="/App" component={App} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/About" component={About} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
+)
